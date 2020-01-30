ed4abd295e64d50ee0c5965da4fede2d81dd9833
William Roger <williamroger.frontend@gmail.com>
2020-01-30 14:47:07 -0300

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -59,7 +59,14 @@
   Mostre no console os números no array.
   */
   console.log('De 0 a 10:');
-  // ?
+  var numbers = [];
+  for (var i = 0; i < 20; i++) {
+    if (i > 10) {
+      break;
+    }
+    numbers.push(i);
+  }
+  console.log(numbers);
 
   /*
   Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
@@ -68,6 +75,12 @@
   Mostrar no console os números do array.
   */
   console.log('Pares de 0 a 20:');
-  // ?
-
+  numbers = [];
+  for (var i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+      continue;
+    }
+    numbers.push(i);
+  }
+  console.log(numbers);
 })();
